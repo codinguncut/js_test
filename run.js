fc96dd60609138d7176605daf9fb8154a40da4d9
Johannes Ahlmann <johannes@scrapinghub.com>
2017-02-17 11:00:09 +0000

    slice 16

--- run.js
+++ run.js
@@ -10,6 +10,6 @@ eval(md5);
 
 if (process.argv.length > 1) {
     var string = process.argv[2];
-    var res = encode_ascii85(hex_md5(string));
+    var res = encode_ascii85(hex_md5(string)).slice(0, 16);
     console.log(res);
 }
