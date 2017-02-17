8c1c62c5eccb711f9ca2a17ca4def4c37d5d137b
Johannes Ahlmann <johannes@scrapinghub.com>
2017-02-17 10:53:25 +0000

    added node js script

---
+++ run.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+b85 = fs.readFileSync('lib.js','utf8');
+eval(b85);
+
+md5 = fs.readFileSync('md5-min.js','utf8');
+eval(md5);
+
+if (process.argv.length > 1) {
+    var string = process.argv[2];
+    var res = encode_ascii85(hex_md5(string).slice(0, 8));
+    console.log(res);
+}
